67834b187523d4ba99327f249d3e495c222208ab
Quentin Klein <qklein@eliocity.com>
2015-02-18 13:43:31 +0100

    Improve javadoc

--- simple-location-tracker/slt/src/main/java/fr/quentinklein/slt/LocationTracker.java
+++ simple-location-tracker/slt/src/main/java/fr/quentinklein/slt/LocationTracker.java
@@ -1,4 +1,4 @@
-package fr.quentinklein.slt.tracker;
+package fr.quentinklein.slt;
 
 import android.content.Context;
 import android.location.Location;
@@ -9,181 +9,188 @@ import android.os.Handler;
 import android.util.Log;
 
 /**
- * @author qklein, yasiralijaved
+ * @author Quentin Klein <klein.quentin@gmail.com>, Yasir.Ali <ali.yasir0@gmail.com>
+ * <p>
  * Helper that tracks user location
+ * </p>
  *
  */
 public abstract class LocationTracker implements LocationListener {
 
-	/**
-	 * Tag used for logs
-	 */
-	private static final String TAG = "LocationTracker";
-	/**
-	 * The user location
-	 * This value is static because, wherever you call a LocationTracker, user location is the same
-	 */
-	private static Location location;
-	
-	/**
-	 * The manager used to track the location
-	 */
-	private LocationManager locationManagerService;
-	/**
-	 * Indicates if Tracker is listening for updates or not
-	 */
-	private boolean isListening = false;
-	/**
-	 * Indicates if Tracker has found the location or not
-	 */
-	private boolean isLocationFound = false;
-	
-	private TrackerSettings mTrackerSettings;
+    /**
+     * Tag used for logs
+     */
+    private static final String TAG = "LocationTracker";
+    /**
+     * The user location
+     * This value is static because, wherever you call a LocationTracker, user location is the same
+     */
+    private static Location location;
 
-	
-	
-	/**
-	 * Customized LocationTracker, uses the specified services and starts listening for a location.
-	 * @param context Android context, uiContext is not mandatory.
-	 * @param trackerSettings {@link TrackerSettings.java}
-	 */
-	public LocationTracker(Context context, TrackerSettings trackerSettings){
-		
-		this.mTrackerSettings = trackerSettings;
-		
-		// Get the location manager
-		this.locationManagerService = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		// default
-		if(location == null && trackerSettings.shouldUseGPS()) {
-			LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		}
-		if(location == null && trackerSettings.shouldUseNetwork()) {
-			LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		}
-		if(location == null && trackerSettings.shouldUsePassive()) {
-			LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-		}
-		// Start Listen for updates
-		startListen();
-	}
+    /**
+     * The manager used to track the location
+     */
+    private LocationManager locationManagerService;
+    /**
+     * Indicates if Tracker is listening for updates or not
+     */
+    private boolean isListening = false;
+    /**
+     * Indicates if Tracker has found the location or not
+     */
+    private boolean isLocationFound = false;
 
-	/**
-	 * Make the tracker listening for location updates
-	 */
-	public final void startListen(){
-		if(!this.isListening) {
-			Log.i(TAG, "LocationTracked is now listening for location updates");
-			// Listen for GPS Updates
-			if(locationManagerService.isProviderEnabled(LocationManager.GPS_PROVIDER) && this.mTrackerSettings.shouldUseGPS()){
-				locationManagerService.requestLocationUpdates(LocationManager.GPS_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
-			} else if(this.mTrackerSettings.shouldUseGPS()){
-				Log.i(TAG, "Problem, GPS_PROVIDER is not enabled");
-			}
-			// Listen for Network Updates
-			if(locationManagerService.isProviderEnabled(LocationManager.NETWORK_PROVIDER) && this.mTrackerSettings.shouldUseNetwork()){
-				locationManagerService.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
-			} else if(this.mTrackerSettings.shouldUseNetwork()){
-				Log.i(TAG, "Problem, NETWORK_PROVIDER is not enabled");
-			}
-			// Listen for Passive Updates
-			if(locationManagerService.isProviderEnabled(LocationManager.PASSIVE_PROVIDER) && this.mTrackerSettings.shouldUsePassive()){
-				locationManagerService.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
-			} else if(this.mTrackerSettings.shouldUsePassive()){
-				Log.i(TAG, "Problem, PASSIVE_PROVIDER is not enabled");
-			}
-			this.isListening = true;
-			
-			new Handler().postDelayed(new Runnable() {
-				  @Override
-				  public void run() {
-				    if(!isLocationFound && isListening && mTrackerSettings.getTimeout() != -1) {
-				      Log.i(TAG, "No location found in the meantime");
-				      LocationTracker.this.stopListen();
-				      onTimeout();
-				    }
-				  }
-				}, mTrackerSettings.getTimeout());
-		} else {
-			Log.i(TAG, "Relax, LocationTracked is already listening for location updates");
-		}
-	}
+    /**
+     * Settings for the tracker
+     */
+    private TrackerSettings mTrackerSettings;
 
-	/**
-	 * Make the tracker stops listening for location updates
-	 */
-	public final void stopListen(){
-		if(this.isListening){
-			Log.i(TAG, "LocationTracked has stopped listening for location updates");
-			locationManagerService.removeUpdates(this);
-			this.isListening = false;
-		} else {
-			Log.i(TAG, "LocationTracked wasn't listening for location updates anyway");
-		}
-	}
-	
-	/**
-	 * Best effort, it calls {@link #onLocationChanged(Location)} with static field named {@link #location} if it is not null
-	 */
-	public final void quickFix(){
-		if(LocationTracker.location != null){
-			onLocationChanged(LocationTracker.location);
-		}
-	}
 
-	/**
-	 * Called when the tracker had found a location
-	 * @see android.location.LocationListener#onLocationChanged(android.location.Location)
-	 */
-	@Override
-	public final void onLocationChanged(Location location) {
-		Log.i(TAG, "Location has changed, new location is "+location);
-		LocationTracker.location = new Location(location);
-		isLocationFound = true;
-		onLocationFound(location);
-	}
+    /**
+     * Customized LocationTracker, uses the specified services and starts listening for a location.
+     * @param context Android context, uiContext is not mandatory.
+     * @param trackerSettings {@link TrackerSettings}
+     */
+    public LocationTracker(Context context, TrackerSettings trackerSettings) {
 
-	/**
-	 * Called when the tracker had found a location
-	 * @param location the found location
-	 */
-	public abstract void onLocationFound(Location location);
-	
-	/**
-	 * Called when the tracker had not found any location and the timeout just happened
-	 */
-	public abstract void onTimeout();
+        this.mTrackerSettings = trackerSettings;
 
-	/** 
-	 * Called when a provider has been disabled.
-	 * By default, this method do nothing but a Log on i
-	 * @see android.location.LocationListener#onProviderDisabled(java.lang.String)
-	 */
-	@Override
-	public void onProviderDisabled(String provider) {
-		// By default do nothing but log
-		Log.i(TAG, "Provider ("+provider+") has been disabled");
-	}
+        // Get the location manager
+        this.locationManagerService = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        // default
+        if (location == null && trackerSettings.shouldUseGPS()) {
+            LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        if (location == null && trackerSettings.shouldUseNetwork()) {
+            LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        if (location == null && trackerSettings.shouldUsePassive()) {
+            LocationTracker.location = locationManagerService.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
+        // Start Listen for updates
+        startListen();
+    }
 
-	/** 
-	 * Called when a provider has been enabled.
-	 * By default, this method do nothing but a Log on i
-	 * @see android.location.LocationListener#onProviderEnabled(java.lang.String)
-	 */
-	@Override
-	public void onProviderEnabled(String provider) {
-		// By default do nothing but log
-		Log.i(TAG, "Provider ("+provider+") has been enabled");		
-	}
+    /**
+     * Make the tracker listening for location updates
+     */
+    public final void startListen() {
+        if (!this.isListening) {
+            Log.i(TAG, "LocationTracked is now listening for location updates");
+            // Listen for GPS Updates
+            if (locationManagerService.isProviderEnabled(LocationManager.GPS_PROVIDER) && this.mTrackerSettings.shouldUseGPS()) {
+                locationManagerService.requestLocationUpdates(LocationManager.GPS_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
+            } else if (this.mTrackerSettings.shouldUseGPS()) {
+                Log.i(TAG, "Problem, GPS_PROVIDER is not enabled");
+            }
+            // Listen for Network Updates
+            if (locationManagerService.isProviderEnabled(LocationManager.NETWORK_PROVIDER) && this.mTrackerSettings.shouldUseNetwork()) {
+                locationManagerService.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
+            } else if (this.mTrackerSettings.shouldUseNetwork()) {
+                Log.i(TAG, "Problem, NETWORK_PROVIDER is not enabled");
+            }
+            // Listen for Passive Updates
+            if (locationManagerService.isProviderEnabled(LocationManager.PASSIVE_PROVIDER) && this.mTrackerSettings.shouldUsePassive()) {
+                locationManagerService.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, this.mTrackerSettings.getTimeBetweenUpdates(), this.mTrackerSettings.getMetersBetweenUpdates(), this);
+            } else if (this.mTrackerSettings.shouldUsePassive()) {
+                Log.i(TAG, "Problem, PASSIVE_PROVIDER is not enabled");
+            }
+            this.isListening = true;
 
-	/** 
-	 * Called when status has changed.
-	 * By default, this method do nothing but a Log on i
-	 * @see android.location.LocationListener#onStatusChanged(java.lang.String, int, android.os.Bundle)
-	 */
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// By default do nothing but log
-		Log.i(TAG, "Provider ("+provider+") status has changed, new status is "+status);	
-	}
+            // If user has set a timeout
+            if (mTrackerSettings.getTimeout() != -1) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!isLocationFound && isListening) {
+                            Log.i(TAG, "No location found in the meantime");
+                            LocationTracker.this.stopListen();
+                            onTimeout();
+                        }
+                    }
+                }, mTrackerSettings.getTimeout());
+            }
+        } else {
+            Log.i(TAG, "Relax, LocationTracked is already listening for location updates");
+        }
+    }
+
+    /**
+     * Make the tracker stops listening for location updates
+     */
+    public final void stopListen() {
+        if (this.isListening) {
+            Log.i(TAG, "LocationTracked has stopped listening for location updates");
+            locationManagerService.removeUpdates(this);
+            this.isListening = false;
+        } else {
+            Log.i(TAG, "LocationTracked wasn't listening for location updates anyway");
+        }
+    }
+
+    /**
+     * Best effort, it calls {@link #onLocationChanged(Location)} with static field named {@link #location} if it is not null
+     */
+    public final void quickFix() {
+        if (LocationTracker.location != null) {
+            onLocationChanged(LocationTracker.location);
+        }
+    }
+
+    /**
+     * Called when the tracker had found a location
+     * @see android.location.LocationListener#onLocationChanged(android.location.Location)
+     */
+    @Override
+    public final void onLocationChanged(Location location) {
+        Log.i(TAG, "Location has changed, new location is " + location);
+        LocationTracker.location = new Location(location);
+        isLocationFound = true;
+        onLocationFound(location);
+    }
+
+    /**
+     * Called when the tracker had found a location
+     * @param location the found location
+     */
+    public abstract void onLocationFound(Location location);
+
+    /**
+     * Called when the tracker had not found any location and the timeout just happened
+     */
+    public abstract void onTimeout();
+
+    /**
+     * Called when a provider has been disabled.
+     * By default, this method do nothing but a Log on i
+     * @see android.location.LocationListener#onProviderDisabled(java.lang.String)
+     */
+    @Override
+    public void onProviderDisabled(String provider) {
+        // By default do nothing but log
+        Log.i(TAG, "Provider (" + provider + ") has been disabled");
+    }
+
+    /**
+     * Called when a provider has been enabled.
+     * By default, this method do nothing but a Log on i
+     * @see android.location.LocationListener#onProviderEnabled(java.lang.String)
+     */
+    @Override
+    public void onProviderEnabled(String provider) {
+        // By default do nothing but log
+        Log.i(TAG, "Provider (" + provider + ") has been enabled");
+    }
+
+    /**
+     * Called when status has changed.
+     * By default, this method do nothing but a Log on i
+     * @see android.location.LocationListener#onStatusChanged(java.lang.String, int, android.os.Bundle)
+     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // By default do nothing but log
+        Log.i(TAG, "Provider (" + provider + ") status has changed, new status is " + status);
+    }
 
 }
--- simple-location-tracker/slt/src/main/java/fr/quentinklein/slt/TrackerSettings.java
+++ simple-location-tracker/slt/src/main/java/fr/quentinklein/slt/TrackerSettings.java
@@ -1,7 +1,7 @@
-package fr.quentinklein.slt.tracker;
+package fr.quentinklein.slt;
 
 /*
- * Copyright (C) 2013 Yasir Ali
+ * Copyright (C) 2013 Yasir Ali, Quentin Klein
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,104 +17,107 @@ package fr.quentinklein.slt.tracker;
  */
 
 /**
- * @author Yasir.Ali <ali.yasir0@gmail.com>
- *
- *	Helps the LocationTracker to set the attributes:
- * 	context Android context, uiContext is not mandatory.
- *  useGPS <ul><li>true if GPS usage is wanted</li><li>false otherwise</li></ul>
- *  useNetwork <ul><li>true if Network usage is wanted</li><li>false otherwise</li></ul>
- *  usePassive <ul><li>true if Passive usage is wanted</li><li>false otherwise</li></ul>
- *  minTimeBetweenUpdates the minimum time interval between location updates in milliseconds
- *  minMetersBetweenUpdates the minimum distance between location updates, in meters
+ * @author Quentin Klein <klein.quentin@gmail.com>, Yasir.Ali <ali.yasir0@gmail.com>
+ * <p>
+ * Helps the LocationTracker to set the attributes:
+ * <ul>
+ *  <li>useGPS <ul><li>true if GPS usage is wanted</li><li>false otherwise</li></ul></li>
+ *  <li>useNetwork <ul><li>true if Network usage is wanted</li><li>false otherwise</li></ul></li>
+ *  <li>usePassive <ul><li>true if Passive usage is wanted</li><li>false otherwise</li></ul></li>
+ *  <li>minTimeBetweenUpdates the minimum time interval between location updates in milliseconds</li>
+ *  <li>minMetersBetweenUpdates the minimum distance between location updates, in meters</li>
+ *  <li>timeout the minimum time delay before the tracker stops scanning for location in milliseconds</li>
+ * </ul>
+ * </p>
  */
 public class TrackerSettings {
-	/**
-	 * The default time interval between location updates 
-	 * Its value is 5 minutes
-	 */
-	public static final long DEFAULT_MIN_TIME_BETWEEN_UPDATES = 5 * 60 * 1000;
-	/**
-	 * The default distance between location updates 
-	 * Its value is 100m
-	 */
-	public static final float DEFAULT_MIN_METERS_BETWEEN_UPDATES = 100;
-	/**
-	 * The default value of timeout that helps to stop the listener if the listener is taking too much time 
-	 * Its value is 1 minutes
-	 */
-	public static final int DEFAULT_TIMEOUT = 1 * 60 * 1000;
-	
-	/**
-	 * The minimum time interval between location updates, in milliseconds by default its value is {@link DEFAULT_MIN_TIME_BETWEEN_UPDATES}
-	 */
-	private long timeBetweenUpdates = -1;
-	/**
-	 * The minimum distance between location updates in meters, by default its value is {@link DEFAULT_MIN_METERS_BETWEEN_UPDATES}
-	 */
-	private float metersBetweenUpdates = -1;
-	/**
-	 * The value of timeout to stop the listener after a specified time in case the listener is unable to get the location for a specified time
-	 */
-	private int timeout = -1;
-	
-	/**
-	 * Specifies if tracker should use the GPS
-	 */
-	private boolean useGPS;
-	/**
-	 * Specifies if tracker should use the Network
-	 */
-	private boolean useNetwork;
-	/**
-	 * Specifies if tracker should use the Passive provider
-	 */
-	private boolean usePassive;
-	
-	public void setTimeBetweenUpdates(long timeBetweenUpdates){
-		this.timeBetweenUpdates = timeBetweenUpdates;
-	}
-	
-	public void setMetersBetweenUpdates(float metersBetweenUpdates){
-		this.metersBetweenUpdates = metersBetweenUpdates;
-	}
-	
-	public void setTimeout(int timeout){
-		this.timeout = timeout;
-	}
-	
-	public void setUseGPS(boolean useGPS){
-		this.useGPS = useGPS;
-	}
-	
-	public void setUseNetwork(boolean useNetwork){
-		this.useNetwork = useNetwork;
-	}
-	
-	public void setUsePassive(boolean usePassive){
-		this.usePassive = usePassive;
-	}
-	
-	public long getTimeBetweenUpdates(){
-		return this.timeBetweenUpdates <= -1 ? DEFAULT_MIN_TIME_BETWEEN_UPDATES : this.timeBetweenUpdates;
-	}
-	
-	public float getMetersBetweenUpdates(){
-		return this.metersBetweenUpdates <= -1 ? DEFAULT_MIN_METERS_BETWEEN_UPDATES : this.metersBetweenUpdates;
-	}
-	
-	public int getTimeout(){
-		return this.timeout <= -1 ? DEFAULT_TIMEOUT : this.timeout;
-	}
-	
-	public boolean shouldUseGPS(){
-		return this.useGPS;
-	}
-	
-	public boolean shouldUseNetwork(){
-		return this.useNetwork;
-	}
-	
-	public boolean shouldUsePassive(){
-		return this.usePassive;
-	}
+    /**
+     * The default time interval between location updates
+     * Its value is 5 minutes
+     */
+    public static final long DEFAULT_MIN_TIME_BETWEEN_UPDATES = 5 * 60 * 1000;
+    /**
+     * The default distance between location updates
+     * Its value is 100m
+     */
+    public static final float DEFAULT_MIN_METERS_BETWEEN_UPDATES = 100;
+    /**
+     * The default value of timeout that helps to stop the listener if the listener is taking too much time
+     * Its value is 1 minutes
+     */
+    public static final int DEFAULT_TIMEOUT = 1 * 60 * 1000;
+
+    /**
+     * The minimum time interval between location updates, in milliseconds by default its value is {@link #DEFAULT_MIN_TIME_BETWEEN_UPDATES}
+     */
+    private long timeBetweenUpdates = -1;
+    /**
+     * The minimum distance between location updates in meters, by default its value is {@link #DEFAULT_MIN_METERS_BETWEEN_UPDATES}
+     */
+    private float metersBetweenUpdates = -1;
+    /**
+     * The value of timeout to stop the listener after a specified time in case the listener is unable to get the location for a specified time
+     */
+    private int timeout = -1;
+
+    /**
+     * Specifies if tracker should use the GPS
+     */
+    private boolean useGPS;
+    /**
+     * Specifies if tracker should use the Network
+     */
+    private boolean useNetwork;
+    /**
+     * Specifies if tracker should use the Passive provider
+     */
+    private boolean usePassive;
+
+    public void setTimeBetweenUpdates(long timeBetweenUpdates) {
+        this.timeBetweenUpdates = timeBetweenUpdates;
+    }
+
+    public long getTimeBetweenUpdates() {
+        return this.timeBetweenUpdates <= 0 ? DEFAULT_MIN_TIME_BETWEEN_UPDATES : this.timeBetweenUpdates;
+    }
+
+    public void setMetersBetweenUpdates(float metersBetweenUpdates) {
+        this.metersBetweenUpdates = metersBetweenUpdates;
+    }
+
+    public float getMetersBetweenUpdates() {
+        return this.metersBetweenUpdates <= 0 ? DEFAULT_MIN_METERS_BETWEEN_UPDATES : this.metersBetweenUpdates;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getTimeout() {
+        return this.timeout <= -1 ? DEFAULT_TIMEOUT : this.timeout;
+    }
+
+    public void setUseGPS(boolean useGPS) {
+        this.useGPS = useGPS;
+    }
+
+    public boolean shouldUseGPS() {
+        return this.useGPS;
+    }
+
+    public void setUseNetwork(boolean useNetwork) {
+        this.useNetwork = useNetwork;
+    }
+
+    public boolean shouldUseNetwork() {
+        return this.useNetwork;
+    }
+
+    public void setUsePassive(boolean usePassive) {
+        this.usePassive = usePassive;
+    }
+
+    public boolean shouldUsePassive() {
+        return this.usePassive;
+    }
 }
